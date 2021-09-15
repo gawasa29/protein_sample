2f9b3a7de0d202897ffdac6f841c066d1d4647fc
gawasa29 <yusei112929@gmail.com>
2021-09-15 13:14:14 +0900

    写真選択機能追加

--- lib/Screen/AddPhotoScreen.dart
+++ lib/Screen/AddPhotoScreen.dart
@@ -1,14 +1,16 @@
 // 写真追加画面
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:protein_sample/HomePage.dart';
 
-import '../HomePage.dart';
-import 'TermsofuseScreen.dart';
-class AddPhotoScreen extends StatefulWidget {
-  @override
-  _AddPhotoScreenState createState() => _AddPhotoScreenState();
-}
-
-class _AddPhotoScreenState extends State<AddPhotoScreen> {
+class AddPhotoScreen extends StatelessWidget {
+  const AddPhotoScreen({Key? key}) : super(key: key);
+  void _handleURLButtonPress(BuildContext context, var type) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => ImageFromGalleryEx(type)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,10 +41,29 @@ class _AddPhotoScreenState extends State<AddPhotoScreen> {
                               fontWeight: FontWeight.w600,
                               color: Colors.black54),
                         )),
-                  Image(
-  image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Mona_Lisa_detail_face.jpg/1024px-Mona_Lisa_detail_face.jpg'),
-),
-                  SizedBox(
+                    MaterialButton(
+                      color: Colors.blue,
+                      child: Text(
+                        "Pick Image from Gallery",
+                        style: TextStyle(
+                            color: Colors.white70, fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () {
+                        _handleURLButtonPress(context, ImageSourceType.gallery);
+                      },
+                    ),
+                    MaterialButton(
+                      color: Colors.blue,
+                      child: Text(
+                        "Pick Image from Camera",
+                        style: TextStyle(
+                            color: Colors.white70, fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () {
+                        _handleURLButtonPress(context, ImageSourceType.camera);
+                      },
+                    ),
+                    SizedBox(
                       height: 100,
                     ),
                     SizedBox(
@@ -56,15 +77,97 @@ class _AddPhotoScreenState extends State<AddPhotoScreen> {
                           primary: Color.fromRGBO(128, 128, 128, 1.0),
                           onPrimary: Colors.white,
                         ),
-                        onPressed: () {Navigator.push(
+                        onPressed: () {
+                          Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) =>MyHomePage()),
-                          );},
+                                builder: (context) => MyHomePage()),
+                          );
+                        },
                         child: const Text('次へ'),
                       ),
                     ),
                   ])),
             ])));
+  }
+}
+
+enum ImageSourceType { gallery, camera }
+
+class ImageFromGalleryEx extends StatefulWidget {
+  final type;
+  ImageFromGalleryEx(this.type);
+
+  @override
+  ImageFromGalleryExState createState() => ImageFromGalleryExState(this.type);
+}
+
+class ImageFromGalleryExState extends State<ImageFromGalleryEx> {
+  var _image;
+  var imagePicker;
+  var type;
+
+  ImageFromGalleryExState(this.type);
+
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    imagePicker = new ImagePicker();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(type == ImageSourceType.camera
+              ? "Image from Camera"
+              : "Image from Gallery")),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 52,
+          ),
+          Center(
+            child: GestureDetector(
+              onTap: () async {
+                var source = type == ImageSourceType.camera
+                    ? ImageSource.camera
+                    : ImageSource.gallery;
+                XFile image = await imagePicker.pickImage(
+                    source: source,
+                    imageQuality: 50,
+                    preferredCameraDevice: CameraDevice.front);
+                setState(() {
+                  _image = File(image.path);
+                });
+              },
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(color: Colors.red[200]),
+                child: _image != null
+                    ? Image.file(
+                        _image,
+                        width: 200.0,
+                        height: 200.0,
+                        fit: BoxFit.fitHeight,
+                      )
+                    : Container(
+                        decoration: BoxDecoration(color: Colors.red[200]),
+                        width: 200,
+                        height: 200,
+                        child: Icon(
+                          Icons.camera_alt,
+                          color: Colors.grey[800],
+                        ),
+                      ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
